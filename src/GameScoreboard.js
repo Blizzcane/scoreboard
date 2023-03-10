c6a5aea99207629e9221bd42f982276cd7f5328d
Blizzcane <blizzcane@gmail.com>
2023-03-09 21:07:05 -0600

    added css

--- src/GameScoreboard.js
+++ src/GameScoreboard.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from "react";
+import "./GameScoreboard.css";
 
 function GameScoreboard({ gameData }) {
-    const [games, setGames] = useState([]);
-    useEffect(() => {
-      if (gameData && gameData.games && gameData.games.length > 0) {
-        setGames(gameData.games);
-      }
-    }, [gameData]);
-
-    // Define the scoreboard function here
-    function scoreboard({homeTeam,awayTeam}) {
-      return `${homeTeam.teamName} ${homeTeam.score} - ${awayTeam.score} ${awayTeam.teamName}`;
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    if (gameData && gameData.games && gameData.games.length > 0) {
+      setGames(gameData.games);
     }
-  
-    return (
-      <div>
-        {games.map((game) => (
-            <div key={game.gameId}> 
-                <h2>{scoreboard(game)}</h2>
-          </div>
-        ))}
-      </div>
-    );
-  }
-  
-  export default GameScoreboard;
-  
+  }, [gameData]);
+
+  // Define the scoreboard function here
+  const scoreboard = ({ homeTeam, awayTeam }) => {
+    return `${homeTeam.teamName} ${homeTeam.score} - ${awayTeam.score} ${awayTeam.teamName}`;
+  };
+  const formatDate = (dateString) => {};
+  return (
+    <div className="scoreboard">
+      <h1>{gameData.gameDate}</h1>
+      {games.map((game) => (
+        <div key={game.gameId} className="game">
+          <h2>{scoreboard(game)}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default GameScoreboard;

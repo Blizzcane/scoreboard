3579488caf2671836e2976a38705dc8f5b40a6f0
Blizzcane <blizzcane@gmail.com>
2023-03-27 22:22:18 -0500

    adding player stats

--- src/TestingGameCard.js
+++ src/TestingGameCard.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ScoreboardHeader from "./ScoreBoardHeaders";
 import ScoreDisplay from "./ScoreDisplay";
 import { nbaTeams } from "./utils/teamInfo";
 
 const TestingGameCard = ({ game }) => {
+  const [accordionShown, setAccordionShown] = useState(false);
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+      if (accordionShown) { 
+      // fetch data here and update the players state
+      fetch(`http://127.0.0.1:5000/boxscore?gameId=${game.gameId}`)
+        .then((response) => response.json())
+        .then((data) => setPlayers(data.game.homeTeam.players));
+    }
+  }, [accordionShown]);
   const scoreboard = ({ homeTeam, awayTeam, gameStatusText, gameId }) => {
     // try {
     //   const homeTeamLogo =
@@ -24,6 +35,9 @@ const TestingGameCard = ({ game }) => {
     const awayTeamName = `${awayTeam.teamCity} ${awayTeam.teamName}`;
     const homeTeamScore = homeTeam.score;
     const awayTeamScore = awayTeam.score;
+    const handleAccordionClick = () => {
+      setAccordionShown(!accordionShown);
+    };
 
     return (
       <div className="accordion text-center m-1 shadow">
@@ -35,6 +49,7 @@ const TestingGameCard = ({ game }) => {
             data-bs-target={`#collapse${gameId}`}
             aria-expanded="true"
             aria-controls={`collapse${gameId}`}
+            onClick={handleAccordionClick}
           >
             <ScoreboardHeader team={homeTeam} logo={homeTeamLogo} />
 
@@ -88,6 +103,7 @@ const TestingGameCard = ({ game }) => {
                   </div>
                 </div>
               </div>
+                        <div className="d-flex flex-column">{players.length > 0 && players.map(player => <span>{player.name}</span>)}</div>
             </div>
           </div>
         </div>

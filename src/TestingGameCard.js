fa7c0fac896857e183d8154a6e26575291b3ac8e
Blizzcane <blizzcane@gmail.com>
2023-03-28 22:14:45 -0500

    editing bootstrap

--- src/TestingGameCard.js
+++ src/TestingGameCard.js
@@ -109,23 +109,27 @@ const TestingGameCard = ({ game }) => {
                 {players.length > 0 &&
                   players.map((player) => (
                     <div className="d-flex flex-row justify-content-between">
-                      <div className="">
-                        <span>{player.jerseyNum}</span>
+                      <div className="d-flex flex-row ">
+                        <span className="">{player.jerseyNum}</span>
                         <span>{player.name}</span>
                       </div>
                       <div className=" ">
-                        <span>
+                        <span className="mx-2">
                           {
                             player.statistics.minutesCalculated.match(
                               durationRegex
                             )[1]
                           }
                         </span>
-                        <span>{player.statistics.points}</span>
+                        <span className="mx-2">{player.statistics.points}</span>
 
-                        <span>{player.statistics.reboundsTotal}</span>
+                        <span className="mx-2">
+                          {player.statistics.reboundsTotal}
+                        </span>
 
-                        <span>{player.statistics.assists}</span>
+                        <span className="mx-2">
+                          {player.statistics.assists}
+                        </span>
                       </div>
                     </div>
                   ))}

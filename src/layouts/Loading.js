39f88d2106c17233e1994acdabc566e718e43254
Kishor Krishnan <Blizzcane@gmail.com>
2023-04-24 17:16:11 -0500

    testing loading page

--- src/layouts/Loading.js
+++ src/layouts/Loading.js
@@ -4,9 +4,9 @@ import "./Loading.css"
 // displays a spline3D model of a basketball
 const Loading = () => {
     return (
-        <div class="container">
-            <spline-viewer url="https://prod.spline.design/eHUK6JmPy79D67Rc/scene.splinecode" class="spline-viewer"></spline-viewer>
-            <div class="text-overlay">Dribbling...</div>
+        <div className="container">
+            <div className="text-overlay">Dribbling...</div>
+            <spline-viewer url="https://prod.spline.design/eHUK6JmPy79D67Rc/scene.splinecode" className="spline-viewer"></spline-viewer>
         </div>
     );
 };

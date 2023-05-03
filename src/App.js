a11bf13e0c7e61585cec9bae3acf2d6f69d77fd4
Kishor Krishnan <Blizzcane@gmail.com>
2023-05-03 14:58:16 -0500

    going to leave it for another day

--- src/App.js
+++ src/App.js
@@ -73,11 +73,10 @@ function App() {
     })
     : <Loading />; 
   return (
-    // <div className="d-flex flex-column justify-content-center align-items-center">
-    //   <h2 className="date date-title my-2">{formattedDate}</h2>
-    //   <GameScoreboard gameData={gameData} scoreBoardUrl={scoreBoardUrl} />
-    // </div> 
-    <Loading/>
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h2 className="date date-title my-2">{formattedDate}</h2>
+      <GameScoreboard gameData={gameData} scoreBoardUrl={scoreBoardUrl} />
+    </div>  
   );
 }
 

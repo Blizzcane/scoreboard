52fe4c94c4623cdf8d2eb7fcf4841a62917726c7
Kishor Krishnan <Blizzcane@gmail.com>
2023-05-03 15:08:53 -0500

    changed animation to react because CSS isn't working

--- src/layouts/Loading.js
+++ src/layouts/Loading.js
@@ -1,21 +1,34 @@
-import React, { useEffect, useRef } from "react";
-import { Application } from '@splinetool/runtime';
+import React, { useEffect, useRef, useState } from "react";
+import { Application } from "@splinetool/runtime";
 import "./Loading.css";
 
-// displays a spline3D model of a basketball
 const Loading = () => {
   const canvasRef = useRef(null);
+  const [dots, setDots] = useState("");
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const app = new Application(canvas);
-    app.load('https://prod.spline.design/AU91FKhexZsGBzae/scene.splinecode');
+    app.load("https://prod.spline.design/AU91FKhexZsGBzae/scene.splinecode");
+  }, []);
+
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setDots((dots) => {
+        if (dots === "...") {
+          return "";
+        } else {
+          return dots + ".";
+        }
+      });
+    }, 500);
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div className="container">
-      <div className="text-overlay">Dribbling...</div>
-      <canvas ref={canvasRef} className="canvas3d" ></canvas>
+      <div className="text-overlay">Dribbling{dots}</div>
+      <canvas ref={canvasRef} className="canvas3d"></canvas>
     </div>
   );
 };
